b71e4271bb256cb6117f4a1ed4c2bd31b279ef3f
Chris Speck <cgspeck@gmail.com>
2021-07-25 17:26:01 +1000

    Dynamically set description, currency

--- includes/process/process_stripe.inc.php
+++ includes/process/process_stripe.inc.php
@@ -4,10 +4,10 @@ if ($_SESSION['prefsStripeEnabled'] != "Y") {
     header("HTTP/1.1 303 See Other");
     header("Location: /index.php?section=pay");
 }
-// include(INCLUDES . 'beerXML/input_beer_xml.inc.php');
 require_once(ROOT . 'vendor/stripe-php-7.91.0/init.php');
+include(DB . 'entries.db.php');
+include(INCLUDES . "constants.inc.php");
 include(INCLUDES . "pay.common.inc.php");
-
 
 if (TESTING) {
     $stripe_sk = $_SESSION['prefsStripeTestPrivateKey'];
@@ -15,60 +15,38 @@ if (TESTING) {
     $stripe_sk = $_SESSION['prefsStripeLivePrivateKey'];
 }
 
+if ($_SESSION['prefsProEdition'] == 1) {
+    $item_name = sprintf(
+        "%s - %s",
+        $_SESSION['brewerBreweryName'],
+        remove_accents($_SESSION['contestName'])
+    );
+} else {
+    $item_name = sprintf(
+        "%s, %s - %s",
+        $_SESSION['brewerLastName'],
+        $_SESSION['brewerFirstName'],
+        remove_accents($_SESSION['contestName'])
+    );
+}
 
-// echo ("TODO: create checkout session");
-// echo (calculate_total_to_pay());
 \Stripe\Stripe::setApiKey($stripe_sk);
 header('Content-Type: application/json');
-$YOUR_DOMAIN = 'http://localhost:4242';
+
+$cancel_url = $base_url . "index.php?section=pay&msg=11";
+$success_url = $base_url . "index.php?section=pay&msg=10";
 
 $checkout_session = \Stripe\Checkout\Session::create([
     'payment_method_types' => ['card'],
     'line_items' => [[
-        'name' => "competition entries",
+        'name' => $item_name,
         'amount' => calculate_total_to_pay() * 100,
         'currency' => 'aud',
         'quantity' => 1,
     ]],
     'mode' => 'payment',
-    'success_url' => $YOUR_DOMAIN . '/success.html',
-    'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+    'success_url' => $success_url,
+    'cancel_url' => $cancel_url,
 ]);
 header("HTTP/1.1 303 See Other");
 header("Location: " . $checkout_session->url);
-// create the stripe checkout session and redirect
-// PayPal settings
-// $paypal_email = $_POST['business'];
-// $return_url = $_POST['return'];
-// $cancel_url = $_POST['cancel_return'];
-// // $notify_url = $base_url."ppv.php";
-// if ((TESTING) || (DEBUG)) $paypal_url = "https://www.sandbox.paypal.com/cgi-bin/webscr";
-// else $paypal_url = "https://www.paypal.com/cgi-bin/webscr";
-
-// $item_name = sterilize($_POST['item_name']);
-// $item_amount = sterilize($_POST['amount']);
-
-// // ---------------------------- Check if paypal request or response  ---------------------------
-
-// // Request payment
-
-// $query_string = "";
-
-// // Append PayPal account to querystring
-// $query_string .= "?business=".urlencode($paypal_email)."&";
-
-// // Loop for posted values and append to querystring
-// foreach($_POST as $key => $value){
-// 	if (($key != "cancel_return") && ($key != "return")) {
-// 		$value = urlencode(stripslashes($value));
-// 		$query_string .= "$key=$value&";
-// 	}
-// }
-
-// // Append paypal return addresses
-// $query_string .= "return=".urlencode(stripslashes($return_url));
-// $query_string .= "&cancel_return=".urlencode(stripslashes($cancel_url));
-// // $query_string .= "&notify_url=".urlencode($notify_url);
-
-// // Redirect to PayPal IPN
-// $redirect_go_to = sprintf('location:%s%s',$paypal_url,$query_string);
