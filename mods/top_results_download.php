3b7872ecaa1ce2bc4513eb372c0ea965f2069bd0
Chris Speck <cgspeck@gmail.com>
2023-07-03 23:05:38 +1000

    Add pdf download widget used last year

---
+++ mods/top_results_download.php
@@ -0,0 +1,25 @@
+<!--
+SOME THINGS TO NOTE:
+- DO NOT include <html> or <body> tags!!
+- All files MUST have a .php extension (e.g., name_of_file.php - some servers running PHP are not configured to include files with other exensions).
+- For the program to use any custom module, its information MUST be added into the database.
+  -- The Custom Modules option must be enabled via Website Preferences
+  -- The EXACT filename and other info (position, rank, description) must be entered into the database via the Add Custom Modules screen
+- The corresponding file should be uploaded to the "mods" sub-folder via secure FTP.
+- Custom modules can only be placed above content (just below the main navigation) or below content (just above the footer).
+- You can have multiple custom modules. They will be displayed in the rank order you choose.
+
+For assistance with Bootstrap elements, reference the Bootstrap website:
+- CSS:        http://getbootstrap.com/css/
+- Components: http://getbootstrap.com/components/
+- JavaScript: http://getbootstrap.com/javascript/
+
+BCOE&M uses Font Awesome v4.7 icons throughout the core code. To use Font Awesome icons, reference the following:
+- Font Awesome icon list:     https://fontawesome.com/v4.7/icons
+- Font Awesome icon examples: https://fontawesome.com/v4.7/examples/
+-->
+<div class="jumbotron">
+  <h2>2022 Full Results List</h2>
+  <p>Click the button below to download the full results list as a PDF.</p>
+  <p><a role="button" class="btn btn-info btn-lg btn-block" href="https://comps.westgatebrewers.org/public/stout-extravaganza-full-result-list.pdf"><span class="fa fa-download"></span>Download</a></p>
+</div>
